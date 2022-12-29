7c3a46ae4ece201ca5cbfbdcdbfd18e96ceb03a5
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-12-29 09:42:52 +0100

    Implements about and 404 page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,6 +1,18 @@
 const AboutPage = () => {
     return (
-        <p>This is react project</p>
+        <div className="page-container">
+            <h1>About website</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam officia minima sapiente eveniet neque, expedita officiis, cupiditate ratione beatae laborum corrupti reprehenderit? Exercitationem libero ad odit dicta. Aspernatur, odit.</p>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam officia minima sapiente eveniet neque, expedita officiis, cupiditate ratione beatae laborum corrupti reprehenderit? Exercitationem libero ad odit dicta. Aspernatur, odit.</p>
+
+            <div className="link-collection">
+                <a href="https://www.google.com/">Link to github</a>
+                <a href="">Some other link</a>
+                <a href="">Yet a third link to some place</a>
+            </div>
+        </div>
     )
 }
 
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,22 @@
+import { Link, useNavigate } from "react-router-dom"
+
 const NotFoundPage = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <p>This page was not found</p>
+        <div className="page-container">
+            <h1>404 Page not found</h1>
+            <p>The web address you entered is not a page on this website. If it worked before, it seems to have been removed or changed. Make sure to check your spelling.</p>
+
+            <h2>Hese is some pages that do exists</h2>
+
+            <div className="link-collection">
+                <Link to='/'>Go to homepage</Link>
+                <Link to='/quizlist'>Go to quiz list</Link>
+                <p className="link" onClick={()=>{navigate(-1)}}>Go back to previous page</p>
+            </div>
+        </div>
     )
 }
 
