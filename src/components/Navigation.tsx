dc85e76e7f8cf02a5f10fb55eaecb8021c51128c
Tim Nilsson <tim.r.nilsson@gmail.com>
2022-12-30 16:23:11 +0100

    Changes layout of navbar on desktop breakpoint

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -80,6 +80,105 @@ const Navigaion = () => {
                         <img src={ isMenuOpen ? menuCloseIcon : menuOpenIcon } alt='menu-toggle-icon' />
                     </div>
                 </div>
+
+                <div className='nav-desktop'>
+                    {
+                        currentUser 
+                        ?
+                        <nav className='link-collection'>
+                            <Link 
+                                to='/quizlist'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                View quiz list
+                            </Link>
+                            <Link 
+                                to='/createquiz'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                Create a quiz
+                            </Link>
+                            <p 
+                                className='link' 
+                                onClick={()=>{signOut();setIsMenuOpen(false)}}
+                            >
+                                logout
+                            </p>
+                            <Link 
+                                to='/about'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                About website
+                            </Link>
+                            
+                        </nav>
+                        :
+                        <nav className='link-collection'>
+                            <Link 
+                                to='/quizlist'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                View quiz list
+                            </Link>
+                            <Link 
+                                to='/signup'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                Sign up
+                            </Link>
+                            <Link 
+                                to='/signin'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                Sign in
+                            </Link>
+                            <Link 
+                                to='/about'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                About website
+                            </Link>
+                        </nav>
+                    }
+                    <div className='nav-top-icon-desktop-wrapper'>
+                        {
+                            currentUser 
+                            ?
+                            <Link 
+                                to={`/profile/${currentUser.uid}`} 
+                                className='nav-avatar-container'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                <img 
+                                    src={currentUser.photoURL ? currentUser.photoURL : defaultAvatar} 
+                                    alt='nav-avatar-image' 
+                                />
+                            </Link>
+                            :
+                            <Link 
+                                to='/signup' 
+                                className='btn btn-info'
+                                onClick={()=>{setIsMenuOpen(false)}}
+                            >
+                                Sign up!
+                            </Link>
+                        }
+                        <form className='search-form' onSubmit={handleSearch} noValidate>
+                            <input 
+                                type="search" 
+                                placeholder='Search...' 
+                                ref={searchInput} 
+                            />
+                            <img 
+                                src={searchIcon} 
+                                alt='search-button' 
+                                role='button' 
+                                onClick={handleSearch} 
+                                className='search-button'
+                            />
+                        </form>
+                    </div>
+                </div>
             </div>
             {
                 isMenuOpen &&
