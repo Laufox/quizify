19c90e1c79af242361b945d1bca3f19e33066184
Tim Nilsson <tim.r.nilsson@gmail.com>
2023-01-02 10:15:06 +0100

    Adds icons to user home page

--- src/pages/UserHomePage.tsx
+++ src/pages/UserHomePage.tsx
@@ -1,11 +1,33 @@
+import { Link } from "react-router-dom"
 import { useAuthContext } from "../contexts/AuthContext"
+
+import listIcon from '../assets/icons/list-icon.svg'
+import defaultAvatar from '../assets/icons/defaultavatar.svg'
+import addIcon from '../assets/icons/add-icon.svg'
 
 const UserHomePage = () => {
 
     const { currentUser } = useAuthContext()
 
     return (
-        <p>Welcome { currentUser.displayName }</p>
+        <div className="page-container">
+            <h1>Welcome { currentUser.displayName }!</h1>
+
+            <div className="user-home-page-links">
+                <Link to='/quizlist' >
+                    <img src={listIcon} />
+                    See list of quizzes
+                </Link>
+                <Link to='/createquiz'>
+                    <img src={addIcon} />
+                    Create a new quiz
+                </Link>
+                <Link to={`/profile/${currentUser.uid}`}>
+                    <img src={defaultAvatar} />
+                    View your profile
+                </Link>
+            </div>
+        </div>
     )
 }
 
