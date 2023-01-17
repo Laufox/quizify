58fc4603a134481b49aea4ad191b73ee1eded346
Tim Nilsson <tim.r.nilsson@gmail.com>
2023-01-17 18:59:18 +0100

    Adds content for guest home page and about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,14 +3,23 @@ const AboutPage = () => {
         <div className="page-container">
             <h1>About website</h1>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam officia minima sapiente eveniet neque, expedita officiis, cupiditate ratione beatae laborum corrupti reprehenderit? Exercitationem libero ad odit dicta. Aspernatur, odit.</p>
+            <p>
+                This website is the results of an examiniation project done over december/january of 2022/2023. The project had a timeline of one week of preparation and four week of executing the actual project. The content and features of this webside reflects the goals set during the preparation week.
+            </p>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quisquam officia minima sapiente eveniet neque, expedita officiis, cupiditate ratione beatae laborum corrupti reprehenderit? Exercitationem libero ad odit dicta. Aspernatur, odit.</p>
+            <p>
+                The Quizify project is built with ReactJS framework, using TypeScript as primary language combined with HTML as markup and SASS/CSS for styling porpuses. The project uses a few libraries imported through npm, such as React-Hook-Form and Classnames, but all markup and styling is self-written.
+            </p>
 
+            <p>
+                To read and handle data, this project uses the Firebase services of Firestore, Authentication and Storage. All authentication methods and credential - handling is therefore under the responsibility of firebase Authentication, and not something that this website handles itself. The Quizify project also uses Firebase Hosting to hold the online version of this project.
+            </p>
+
+            <h2>Various external links related to this project</h2>
             <div className="link-collection">
-                <a href="https://www.google.com/">Link to github</a>
-                <a href="">Some other link</a>
-                <a href="">Yet a third link to some place</a>
+                <a href="https://github.com/Laufox/quizify" target='_blank'>GitHub repo for source code</a>
+                <a href="https://reactjs.org/" target='_blank'>Read more about ReactJS</a>
+                <a href="https://firebase.google.com/" target='_blank'>Read more about Firebase</a>
             </div>
         </div>
     )
--- src/pages/GuestHomePage.tsx
+++ src/pages/GuestHomePage.tsx
@@ -4,33 +4,55 @@ const GuestHomePage = () => {
     return (
         <div className="page-container">
             <div className='homepage-guest-intro'>
-                <h1>Quizify, the ultimate quiz</h1>
+                <h1>Quizify, the ultimate quiz application</h1>
 
                 <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic amet odio harum a distinctio quasi incidunt autem provident recusandae adipisci obcaecati, voluptatem quas sint, consectetur porro nihil soluta inventore neque.
+                    Welcome to Quizify, where you have the chance to try out your knowledge about a wide range of categories and topics, all of them defined and written by other quizzers! Everyone can find and play quizzes published, but with an account you can also create a new quiz, within a topic and amount of questions of your own choosing.
                 </p>
             </div>
 
             <div className='homepage-guest-cta'>
                 <h2>Take the chance, join now!</h2>
 
-                <Link to='/signup' className='btn btn-info'>Sign Up!</Link>
+                <div className='homepage-buttons'>
+                    <Link to='/signup' className='btn btn-info'>Sign Up!</Link>
+                    <Link to='/login' className='btn btn-info-open'>Sign in</Link>
+                </div>
             </div>
 
             <div className='homepage-guest-features'>
-                <h2>Features</h2>
+                <h2>What this site features:</h2>
 
                 <div className='features-container'>
-                    <span className='feature-create'>Create!</span>
-                    <span className='feature-play'>Play!</span>
-                    <span className='feature-compete'>Compete!</span>
+                    <div className='feature feature-create'>
+                        <h3>Create!</h3>
+                        <p>
+                            Make your own set of questions to challenge others about knowledge within a category of your choosing! You can set your quizzes to be either public or private, making it possible to create a quiz for any target group, be it your family, work group or the entire planet!
+                        </p>
+                    </div>
+                    <div className='feature feature-play'>
+                        <h3>Play!</h3>
+                        <p>
+                            Test your knowledge in any of the user - genereated quizzes published on this site! Find a quiz suitable for you, by searching through the public list of quizzes, filterable by category.
+                        </p>
+                        <Link to='quizlist'>List of public quizzes can be found here</Link>
+                    </div>
+                    <div className='feature feature-compete'>
+                        <h3>Compete!</h3>
+                        <p>
+                            The score of your previously played quizzes are saved in your profile, and you can always try it again to improve your score. Or why not see if you can perform better than your firends?
+                        </p>
+                    </div>
                 </div>
             </div>
 
             <div className='homepage-guest-cta'>
                 <h2>Don't miss out, join us!</h2>
 
-                <Link to='/signup' className='btn btn-info'>Sign Up!</Link>
+                <div className='homepage-buttons'>
+                    <Link to='/signup' className='btn btn-info'>Sign Up!</Link>
+                    <Link to='/login' className='btn btn-info-open'>Sign in</Link>
+                </div>
             </div>
         </div>
     )
